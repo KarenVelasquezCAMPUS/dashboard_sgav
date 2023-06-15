e3019f98e64b6feba9f712c29ad810d4fff98aeb
KarenVelasquezCAMPUS <karenvelasquez1009@gmail.com>
2023-06-15 08:28:24 -0500

    feat: :memo: ccr js

---
+++ Templates/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
+	<link rel="stylesheet" href="css/style.css"> 
+	<link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+	<script src="main.js" async></script>
+	<script src="app.js" defer type="module"></script>
+	<script src="js/bootstrap/js/bootstrap.bundle.min.js" defer></script>
+	<title>dashboard sgav</title>
+</head>
+<body>
